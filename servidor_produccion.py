e484134efaf9f6f387ce855d3cfaee7137fafd62
ENIER <redennier.dev@gmail.com>
2026-08-11 14:00:04 -0400

    v1.1.13 - Add CSRF_TRUSTED_ORIGINS and Waitress threads optimization

--- servidor_produccion.py
+++ servidor_produccion.py
@@ -5,5 +5,5 @@ from LuxuryMonsei.wsgi import application
 if __name__ == '__main__':
     print("Iniciando el servidor de producción para MET-ATELIER en el puerto 8080...")
     print("Por favor, NO cierres esta ventana. Si se cierra, la página dejará de funcionar.")
-    # Escuchar en todas las interfaces de red (0.0.0.0) en el puerto 8080
-    serve(application, host='0.0.0.0', port=8080)
+    # Escuchar en todas las interfaces de red (0.0.0.0) en el puerto 8080 con 8 hilos
+    serve(application, host='0.0.0.0', port=8080, threads=8)
